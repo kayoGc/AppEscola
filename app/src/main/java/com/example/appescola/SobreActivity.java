8e434eb3e025a8944cc01d0350aff4e59bbba3fc
kayovGc <kayivicti@gmail.com>
2023-03-09 21:14:07 -0300

    fazendo arquivos activity - layout

---
+++ app/src/main/java/com/example/appescola/SobreActivity.java
@@ -0,0 +1,14 @@
+package com.example.appescola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SobreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sobre_layout);
+    }
+}
---
+++ app/src/main/java/com/example/appescola/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.appescola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+    }
+}
